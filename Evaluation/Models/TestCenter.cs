2438b3c97dd7a26f9f41ce4f55d3690a1877e034
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-04-11 17:30:23 +0430

    Deleted unused models and controller

--- Evaluation/Models/TestCenter.cs
+++
@@ -1,9 +0,0 @@
-namespace Evaluation.Models
-{
-	public class TestCenter
-	{
-		public int Id { get; set; }
-		public string MTitle { get; set; }
-		public string Address { get; set; }
-	}
-}
--- Evaluation/Models/TestSection.cs
+++
@@ -1,9 +0,0 @@
-namespace Evaluation.Models
-{
-	public class TestSection
-	{
-		public int Id { get; set; }
-		public string MTitle { get; set; }
-		public int FkCenter { get; set; }
-	}
-}
