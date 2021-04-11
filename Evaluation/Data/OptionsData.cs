9f61a04f4de1ccb1b19e55aaf6bcbe4a35e28977
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2021-04-11 17:32:45 +0430

    Fixed the options data

---
+++ Evaluation/Data/OptionsData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Evaluation.Data
+{
+	public static class OptionsData
+	{
+		public static Dictionary<string, List<Option>> AdminOptions = new Dictionary<string, List<Option>>
+		{
+			{
+				"BasicInformation",
+				new List<Option>
+				{
+					new Option {Href = "/information/center", DisplayName = "مرکز"},
+					new Option {Href = "/information/section", DisplayName = "بخش"},
+					new Option {Href = "/information/position", DisplayName = "سمت"},
+					new Option {Href = "/information/staff", DisplayName = "کارمند"},
+					new Option {Href = "/information/staffPosition", DisplayName = "سمت کارمند"}
+			}
+			},
+			{
+				"Question",
+				new List<Option>
+				{
+					new Option {Href = "/question/detail", DisplayName = "پرسش"},
+					new Option {Href = "/question/section", DisplayName = "پرسش مرکز"}
+				}
+			},
+			{
+				"UserSetting",
+				new List<Option>
+				{
+					new Option {Href = "/user/changePassword", DisplayName = "تغییر رمز عبور"}
+				}
+			},
+			{
+				"Report",
+				new List<Option>
+				{
+					new Option {Href = "/report/center", DisplayName = "مرکز"},
+					new Option {Href = "/report/section", DisplayName = "بخش"},
+					new Option {Href = "/report/position", DisplayName = "سمت"},
+					new Option {Href = "/report/staff", DisplayName = "کارمند"}
+				}
+			}
+		};
+	}
+
+	public class Option
+	{
+		public string Href { get; set; }
+		public string DisplayName { get; set; }
+	}
+}
